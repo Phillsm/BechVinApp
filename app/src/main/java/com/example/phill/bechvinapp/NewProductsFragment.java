40df05aa0d92b4419dcb1ef303372234df765b72
bastianbuhrkall <buhrkall121291>
2015-05-27 12:50:00 +0200

    added functionality, add new order.

--- app/src/main/java/com/example/phill/bechvinapp/NewProductsFragment.java
+++ app/src/main/java/com/example/phill/bechvinapp/NewProductsFragment.java
@@ -1,6 +1,7 @@
 package com.example.phill.bechvinapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,15 +10,22 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.phill.bechvinapp.DataSource.ApiHelper;
+import com.example.phill.bechvinapp.DataSource.DataSourceFacade;
+import com.example.phill.bechvinapp.Model.Order;
 import com.example.phill.bechvinapp.Model.Product;
 import com.example.phill.bechvinapp.dummy.DummyContent;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A fragment representing a list of Items.
@@ -28,7 +36,7 @@ import java.util.ArrayList;
  * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
  * interface.
  */
-public class NewProductsFragment extends Fragment implements AbsListView.OnItemClickListener {
+public class NewProductsFragment extends Fragment implements AbsListView.OnItemClickListener, View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -51,6 +59,7 @@ public class NewProductsFragment extends Fragment implements AbsListView.OnItemC
      * Views.
      */
     private ListAdapter mAdapter;
+    private HashMap<Product, Integer> productsForOrder = new HashMap<>();
 
     // TODO: Rename and change types of parameters
     public static NewProductsFragment newInstance(String param1, String param2) {
@@ -77,6 +86,9 @@ public class NewProductsFragment extends Fragment implements AbsListView.OnItemC
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
 
         // TODO: Change Adapter to display your content
 
@@ -111,11 +123,17 @@ public class NewProductsFragment extends Fragment implements AbsListView.OnItemC
         View view = inflater.inflate(R.layout.fragment_newproducts, container, false);
 
         // Set the adapter
+
+        Button orderAddButton = (Button) view.findViewById(R.id.placeOrderButton);
+        orderAddButton.setOnClickListener(this);
+
         mListView = (AbsListView) view.findViewById(R.id.listtest);
         ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
 
         // Set OnItemClickListener so we can be notified on item clicks
         mListView.setOnItemClickListener(this);
+
+
 
         return view;
     }
@@ -138,12 +156,27 @@ public class NewProductsFragment extends Fragment implements AbsListView.OnItemC
     }
 
 
+
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         if (null != mListener) {
-            // Notify the active callbacks interface (the activity, if the
-            // fragment is attached to one) that an item has been selected.
-            mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
+
+
+
+            ApiHelper api = new ApiHelper(getActivity().getBaseContext());
+            ArrayList<Product> products = api.getAllWines();
+
+
+            mListener.onFragmentInteraction(products.get(position).toString());
+
+
+            Context context = getActivity().getApplicationContext();
+            CharSequence text = "Added!  " + products.get(position).getName();
+            int duration = Toast.LENGTH_SHORT;
+
+            productsForOrder.put(products.get(position), 1);
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
     }
 
@@ -159,6 +192,25 @@ public class NewProductsFragment extends Fragment implements AbsListView.OnItemC
             ((TextView) emptyView).setText(emptyText);
         }
     }
+
+    @Override
+    public void onClick(View v) {
+
+        Order o = new Order(MainActivity.costumerNumber,productsForOrder,new Date(),"attperson");
+        DataSourceFacade dsf = new DataSourceFacade(getActivity().getBaseContext());
+
+        dsf.saveOrder(o);
+
+        Context context = getActivity().getApplicationContext();
+        CharSequence text = "Added!  " + o.toString();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+    }
+
 
     /**
      * This interface must be implemented by activities that contain this
